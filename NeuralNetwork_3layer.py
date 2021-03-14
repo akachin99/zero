53b8ac53578a7da939aa9f7ae628c7a81721b03e
akachin99 <shouichiro_o@yahoo.co.jp>
2021-03-14 19:48:42 +0900

    修正

--- NeuralNetwork_3layer.py
+++ NeuralNetwork_3layer.py
@@ -13,12 +13,13 @@ class NN_3L_func:
 
 	def init_network(self):
 		self.network = {}
-		self.network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2], [0.4], [0.6]])
-		self.network['W2'] = np.array([[0.1, 0.3], [0.2], [0.4]])
-		self.network['W3'] = np.array([[0.1, 0.3], [0.2], [0.4]])
+		self.network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+		self.network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+		self.network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
 		self.network['B1'] = np.array([0.1, 0.2, 0.3])
 		self.network['B2'] = np.array([0.1, 0.2])
 		self.network['B3'] = np.array([0.1, 0.2])
+
 		return self.network
 
 	def forward(self, network, x):
@@ -26,12 +27,16 @@ class NN_3L_func:
 		self.W1 = network['W1']
 		self.W2 = network['W2']
 		self.W3 = network['W3']
+#		self.b1, self.b2, self.b3 = network['B1'], network['B2'], network['B3']
+		self.b1 = network['B1']
+		self.b2 = network['B2']
+		self.b3 = network['B3']
 
-		self.a1 = np.dot(self.W1, x) + self.b1
-		self.z1 = sigmoid(self.a1)
-		self.a2 = np.dot(self.W2, self.z1) + self.b2
-		self.z2 = sigmoid(self.a2)
-		self.a3 = np.dot(self.W3, self.z2) + self.b3
-		self.y = identify_function(self.a3)
+		self.a1 = np.dot(x, self.W1) + self.b1
+		self.z1 = self.sigmoid(self.a1)
+		self.a2 = np.dot(self.z1, self.W2) + self.b2
+		self.z2 = self.sigmoid(self.a2)
+		self.a3 = np.dot(self.z2, self.W3) + self.b3
+		self.y = self.identify_function(self.a3)
 
-		return y
+		return self.y
